b18c8d7504e28ed504ee61cdad18b0f96258a0dd
anees <aneesk002@gmail.com>
2026-05-25 11:19:13 +0530

    ssd

---
+++ index.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+const bot = new TelegramBot(
+    process.env.BOT_TOKEN,
+    {
+        polling: true
+    }
+);
+
+console.log("Bot is running...");
+
+const TOPICS = {
+    4: "arrays",
+    6: "strings",
+    7: "linkedlist",
+    8: "stackandqueue",
+    9: "trees",
+    10: "graphs",
+    11: "dynamic-programming"
+};
+
+bot.on('message', async (msg) => {
+
+    try {
+        const text = msg.text || "";
+        if (!text) return;
+
+        const topicName =
+            TOPICS[msg.message_thread_id];
+
+        if (!topicName) return;
+
+        const hashtags =
+            text.match(/#\w+/g) || [];
+
+        const links =
+            text.match(/https?:\/\/\S+/g) || [];
+
+        const resourceType =
+            hashtags[0]?.replace('#', '') || "unknown";
+
+        const lines = text
+            .split('\n')
+            .filter(line => line.trim() !== "");
+
+        const title =
+            lines[1] || "Untitled";
+
+        const link =
+            links[0] || null;
+
+        const { data, error } = await supabase
+            .from('resources')
+            .insert([
+                {
+                    topic: topicName,
+                    resource_type: resourceType,
+                    title: title,
+                    link: link,
+                    message: text,
+                    telegram_message_id:msg.message_id,
+                    telegram_topic_id:msg.message_thread_id
+                }
+            ]);
+        if (error) {
+            console.log("SUPABASE ERROR:");
+            console.log(error);
+        } else {
+            console.log("RESOURCE SAVED");
+            console.log({
+                topic: topicName,
+                resource_type: resourceType,
+                title: title,
+                link: link
+            });
+        }
+    } catch (err) {
+        console.log("SERVER ERROR:");
+        console.log(err);
+    }
+});
