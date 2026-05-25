3ac762c19ac3e296d36b918e2aa0e6541599b72c
anees <aneesk002@gmail.com>
2026-05-25 11:27:49 +0530

    asd

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Telegram Bot Running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
 const TelegramBot = require('node-telegram-bot-api');
 const { createClient } = require('@supabase/supabase-js');
 
